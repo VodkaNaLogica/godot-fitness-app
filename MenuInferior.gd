6bbc390a6f451973880d5bec177ffce09fe52931
P4 <p4bl0eduardo2011@gmail.com>
2026-08-17 10:46:53 -0300

    Preciso de alguma solução para a meta semanal

--- MenuInferior.gd
+++ MenuInferior.gd
@@ -2,3 +2,9 @@ extends Control
 
 func _on_perfil_button_up() -> void:
 	get_tree().change_scene_to_file("res://Perfil.tscn")
+
+func _on_progresso_button_up() -> void:
+	get_tree().change_scene_to_file("res://Progresso.tscn")
+
+func _on_inicio_button_up() -> void:
+	get_tree().change_scene_to_file("res://Inicio.tscn")
